c20cd6afcc75d72def60dd0744ab274ae809a64b
alex-daley <adaley.gamedev@gmail.com>
2021-01-31 10:43:59 +0000

    Emoji accordion display

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,46 @@
 import React from 'react';
 import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
+import makeStyles from '@material-ui/core/styles/makeStyles';
+import groupBy from 'lodash/groupBy';
+import startCase from 'lodash/startCase';
+import EmojiAccordion from './EmojiAccordion';
+
+const useStyles = makeStyles(theme => ({
+  accordionContainer: {
+    width: '100%',
+    marginTop: theme.spacing(4)
+  }
+}));
+
+function groupEmojis(emojis) {
+  const groups = groupBy(emojis, emoji => emoji.group);
+  return Object.entries(groups);
+}
+
+function titleCase(title) {
+  // HACK: .replace for 'and' correction...
+  return startCase(title).replace('And', 'and');
+}
 
 function App({ emojis }) {
+  const classes = useStyles();
+  const emojiGroups = groupEmojis(emojis);
+
   return (
-    <Container maxWidth="md">
+    <Container maxWidth="xs">
       <Grid container spacing={2}>
-        
+        <div className={classes.accordionContainer}>
+          {emojiGroups.map(([name, emojis], i) => {
+            return (
+              <EmojiAccordion
+                key={i}
+                title={titleCase(name)}
+                emojis={emojis}
+              />
+            )
+          })}
+        </div>
       </Grid>
     </Container>
   );
---
+++ src/components/EmojiAccordion.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import Accordion from '@material-ui/core/Accordion';
+import AccordionSummary from '@material-ui/core/AccordionSummary';
+import AccordionDetails from '@material-ui/core/AccordionDetails';
+import Typography from '@material-ui/core/Typography';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+
+function EmojiGlyph({ emoji }) {
+  const style = {
+    display: 'inline-block', 
+    width: '24px'
+  };
+
+  return <span style={style}>{emoji.glyph}</span>
+}
+
+function EmojiAccordion({ title, emojis }) {
+  return (
+    <Accordion>
+      <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+        <Typography>
+          {title}
+        </Typography>
+      </AccordionSummary>
+      <AccordionDetails>
+        <Typography>
+          {emojis.map(emoji => <EmojiGlyph emoji={emoji}/>)}
+        </Typography>
+      </AccordionDetails>
+    </Accordion>
+  )
+}
+
+export default EmojiAccordion;
